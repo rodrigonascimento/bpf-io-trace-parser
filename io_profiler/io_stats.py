103d3c5bcd2d6927777e6bc8d9a410d2322d0e94
Nascimento, Rodrigo <rodrigon@netapp.com>
2024-03-06 19:48:49 -0300

    ref: added per-file stats parsing.

--- io_profiler/io_stats.py
+++ io_profiler/io_stats.py
@@ -17,7 +17,7 @@ class GlobalIOStats():
             self.global_syscall_counts[syscall_name] += 1
 
     def write_output_file(self):
-        with open('global_syscall_counts.json', 'w') as global_syscounts_output:
+        with open(file='global_syscall_counts.json', mode='w') as global_syscounts_output:
             json.dump(self.global_syscall_counts, global_syscounts_output)
 
 class PerFileIOStats():
@@ -28,4 +28,19 @@ class PerFileIOStats():
     def receive(self, message: str):
         if message[:4] != 'time':
             return
-        
+
+        filename = message.split()[[field.startswith('filename') for field in message.split()].index(True)].split('=')[1]
+        syscall_name = message.split()[[field.startswith('probe') for field in message.split()].index(True)].split('=')[1].split('_')[2]
+
+        if filename not in self.per_file_syscall_counts:
+            self.per_file_syscall_counts[filename] = dict()
+            self.per_file_syscall_counts[filename][syscall_name] = 1
+        else:
+            if syscall_name not in self.per_file_syscall_counts[filename]:
+                self.per_file_syscall_counts[filename][syscall_name] = 1
+            else:
+                self.per_file_syscall_counts[filename][syscall_name] += 1
+
+    def write_output_file(self):
+        with open(file='per_file_syscall_counts.json', mode='w') as per_file_syscounts_output:
+            json.dump(self.per_file_syscall_counts, per_file_syscounts_output)
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from pathlib import Path
 from io_profiler.file_reader import TraceFileReader
-from io_profiler.io_stats import GlobalIOStats
+from io_profiler.io_stats import GlobalIOStats, PerFileIOStats
 
 def main():
-    # Instanciate the trace file reader (publisher)
+    # Instantiate the trace file reader (publisher)
     bpf_trfile_reader = TraceFileReader(Path('test_file.out'))
 
-    # Instanciate the subscribers
+    # Instantiate the subscribers
     global_io_stats = GlobalIOStats(name='global_stats')
+    per_file_io_stats = PerFileIOStats(name='per-file-stats')
 
     # Register the subscribers
     bpf_trfile_reader.subscribe(global_io_stats)
+    bpf_trfile_reader.subscribe(per_file_io_stats)
 
     # Read file and publish each line
     bpf_trfile_reader.read_file_line()
 
     global_io_stats.write_output_file()
+    per_file_io_stats.write_output_file()
 
 if __name__ == '__main__':
     main()
