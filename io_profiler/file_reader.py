c4bfb22d7b5c71b58e65584901e7968c13803661
Nascimento, Rodrigo <rodrigon@netapp.com>
2024-03-08 23:59:55 -0300

    ref: removed unnecessary queue.

--- io_profiler/file_reader.py
+++ io_profiler/file_reader.py
@@ -1,17 +1,14 @@
-from queue import Queue
 from pathlib import Path
 
 class TraceFileReader():
     def __init__(self, trace_filename: Path) -> None:
         self.trace_file = trace_filename
-        self.file_line_queue = Queue()
         self.subscribers = list()
 
     def subscribe(self, subscriber):
         self.subscribers.append(subscriber)
 
     def _publish(self, message: str):
-        self.file_line_queue.put(message)
         for subscriber in self.subscribers:
             subscriber.process(message)
 
@@ -19,4 +16,3 @@ class TraceFileReader():
         with open(file=self.trace_file.name, mode='r') as bpf_output:
             for line in bpf_output.readlines():
                 self._publish(line)
-
