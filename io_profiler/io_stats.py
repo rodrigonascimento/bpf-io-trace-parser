8ed6a336f23ec29f0bceaf85eb06fbc774044439
Nascimento, Rodrigo <rodrigon@netapp.com>
2024-06-10 19:50:52 -0300

    ref: added global pid/tid info.

--- io_profiler/io_stats.py
+++ io_profiler/io_stats.py
@@ -25,6 +25,31 @@ class GlobalSysCallCount():
         with open(file='global_syscall_counts.json', mode='w') as global_syscounts_output:
             json.dump(self.global_syscall_counts, global_syscounts_output, indent=2)
 
+class GlobalPidTidInfo():
+    def __init__(self, name: str, dir_name: str) -> None:
+        self.global_pid_tid_count = dict()
+        self.name = name
+        self.dir_name = dir_name
+
+    def process(self, message: str):
+        if message[:4] != 'time': 
+            return
+        
+        filename = message.split()[[field.startswith('filename') for field in message.split()].index(True)].split('=')[1]
+        if filename.startswith(self.dir_name):
+            pid = int(message.split()[[field.startswith('pid') for field in message.split()].index(True)].split('=')[1])
+            tid = int(message.split()[[field.startswith('tid') for field in message.split()].index(True)].split('=')[1])
+            if pid in self.global_pid_tid_count.keys():
+                if not tid in self.global_pid_tid_count[pid]:
+                    self.global_pid_tid_count[pid].append(tid)
+            else:
+                self.global_pid_tid_count[pid] = list()
+                self.global_pid_tid_count[pid].append(tid) 
+    
+    def write_output_file(self):
+        with open(file='global_pid_tid_info.json', mode='w') as g_pidtid_info:
+            json.dump(self.global_pid_tid_count, g_pidtid_info, indent=2)
+
 class PerFileSysCallCount():
     def __init__(self, name: str, dir_name: str) -> None:
         self.per_file_syscall_counts = dict()
--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from pathlib import Path
 from io_profiler.file_reader import TraceFileReader
-from io_profiler.io_stats import GlobalSysCallCount, PerFileSysCallCount
+from io_profiler.io_stats import GlobalSysCallCount, GlobalPidTidInfo, PerFileSysCallCount
 from io_profiler.telemetry import TelemetryContentRouter
 
 def main():
     bpf_trfile_reader = TraceFileReader(Path('test_file.out'))
 
-    global_io_stats = GlobalSysCallCount(name='g-syscall-count', dir_name='/data/db')
-    per_file_io_stats = PerFileSysCallCount(name='pf-syscall-count', dir_name='/data/db')
+    global_syscall_stats = GlobalSysCallCount(name='g-syscall-count', dir_name='/data/db')
+    global_pid_tid_info = GlobalPidTidInfo(name='g-pidtid-info', dir_name='/data/db')
+    
+    per_file_syscall_stats = PerFileSysCallCount(name='pf-syscall-count', dir_name='/data/db')
+    
     telemetry_content_router = TelemetryContentRouter(name='tlt-router', dir_name='/data/db')
 
-    bpf_trfile_reader.subscribe(global_io_stats)
-    bpf_trfile_reader.subscribe(per_file_io_stats)
+    bpf_trfile_reader.subscribe(global_syscall_stats)
+    bpf_trfile_reader.subscribe(global_pid_tid_info)
+    bpf_trfile_reader.subscribe(per_file_syscall_stats)
     bpf_trfile_reader.subscribe(telemetry_content_router)
 
     bpf_trfile_reader.read_file()
-
-    global_io_stats.write_output_file()
-
-    per_file_io_stats.calculate_total_calls_per_file()
-    per_file_io_stats.write_output_file()
+    
+    global_syscall_stats.write_output_file()
+    global_pid_tid_info.write_output_file()
+    
+    per_file_syscall_stats.calculate_total_calls_per_file()
+    per_file_syscall_stats.write_output_file()\
+    
 
 if __name__ == '__main__':
     main()
