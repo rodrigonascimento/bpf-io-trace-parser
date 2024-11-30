4e56657e33e34df4466126bcb5a13260a93eb420
Nascimento, Rodrigo <rodrigon@netapp.com>
2024-11-30 06:34:13 -0300

    ref: added the per-file content router perFileEvents class.

--- io_profiler/per_file_io_stats.py
+++ io_profiler/per_file_io_stats.py
@@ -1,5 +1,43 @@
 import json
 from pathlib import Path
+from io_profiler.telemetry import TelemetryFile
+
+class PerFileEvents():
+    def __init__(self, name: str, tlt_dir: str):
+        self.name = name
+        self.tlt_dir = tlt_dir
+        self.fd_filename = dict()
+    
+    def process(self, message: str): 
+        if message[:4] != 'time': 
+            return
+        
+        filename       = message.split()[[field.startswith('filename') for field in message.split()].index(True)].split('=')[1]
+        timestamp      = message.split()[[field.startswith('time') for field in message.split()].index(True)].split('=')[1].split('.')[0]
+        ms             = message.split()[[field.startswith('time') for field in message.split()].index(True)].split('=')[1].split('.')[1]
+        probe          = message.split()[[field.startswith('probe') for field in message.split()].index(True)].split('=')[1].split('_')[2]
+        process        = message.split()[[field.startswith('process') for field in message.split()].index(True)].split('=')[1]
+        pid            = message.split()[[field.startswith('pid') for field in message.split()].index(True)].split('=')[1]
+        tid            = message.split()[[field.startswith('tid') for field in message.split()].index(True)].split('=')[1]
+        fd             = message.split()[[field.startswith('fd') for field in message.split()].index(True)].split('=')[1]        
+        lat_ns = message.split()[[field.startswith('lat') for field in message.split()].index(True)].split('=')[1] if 'lat=' in message else ''
+        req_size_bytes = message.split()[[field.startswith('req_size_bytes') for field in message.split()].index(True)].split('=')[1] if 'req_size_bytes=' in message else ''
+        offset         = message.split()[[field.startswith('offset') for field in message.split()].index(True)].split('=')[1] if 'offset' in message else ''
+        if 'read' in probe:
+            bytes_rw   = message.split()[[field.startswith('bytes_read') for field in message.split()].index(True)].split('=')[1]
+        elif 'write' in probe:
+            bytes_rw   = message.split()[[field.startswith('bytes_written') for field in message.split()].index(True)].split('=')[1]
+        else:
+            bytes_rw   = ''
+        
+        line = timestamp + ',' + ms + ',' + probe + ',' + process + ',' + pid + ',' + tid + ',' + filename + ',' + fd + ',' + lat_ns + ',' + req_size_bytes + ',' + offset + ',' + bytes_rw
+        
+        tlt_filename = 'tlt' + filename.replace('/', '_') + '_' + fd +'.csv'
+        if (pid,tid,fd,filename) not in self.fd_filename:
+            tlt_csv_file = TelemetryFile(csv_filename=self.tlt_dir + '/' + tlt_filename)
+            self.fd_filename[(pid,tid,fd,filename)] = tlt_csv_file
+                
+        self.fd_filename[(pid,tid,fd,filename)].add_line(line=line)
 
 class PerFileSysCallCount():
     def __init__(self, name: str, dir_name: str) -> None:
@@ -37,4 +75,3 @@ class PerFileSysCallCount():
     def write_output_file(self, output_file: str):
         with open(file=output_file, mode='w') as per_file_syscounts_output:
             json.dump(self.per_file_syscall_counts, per_file_syscounts_output, indent=2)
-
