b1b0ffe16a525ef70e5dd0d9c83d709df96fbef4
Nascimento, Rodrigo <rodrigon@netapp.com>
2024-11-30 13:20:10 -0300

    ref: added per-file syscall counter.

--- io_profiler/per_file_io_stats.py
+++ io_profiler/per_file_io_stats.py
@@ -40,29 +40,25 @@ class PerFileEvents():
         self.fd_filename[filename].add_line(line=line)
 
 class PerFileSysCallCount():
-    def __init__(self, name: str, dir_name: str) -> None:
+    def __init__(self, name: str) -> None:
         self.per_file_syscall_counts = dict()
         self.name = name
-        self.dir_name = dir_name
 
     def process(self, message: str):
         if message[:4] != 'time':
             return
 
-        filename = message.split()[[field.startswith('filename') for field in message.split()].index(True)].split('=')[1]
-        if filename.startswith(self.dir_name):
-            syscall_name = message.split()[[field.startswith('probe') for field in message.split()].index(True)].split('=')[1].split('_')[2]
+        filename        = message.split()[[field.startswith('filename') for field in message.split()].index(True)].split('=')[1]
+        syscall_name    = message.split()[[field.startswith('probe') for field in message.split()].index(True)].split('=')[1].split('_')[2]
 
-            if filename not in self.per_file_syscall_counts:
-                self.per_file_syscall_counts[filename] = dict()
+        if filename not in self.per_file_syscall_counts:
+            self.per_file_syscall_counts[filename] = dict()
+            self.per_file_syscall_counts[filename][syscall_name] = 1
+        else:
+            if syscall_name not in self.per_file_syscall_counts[filename]:
                 self.per_file_syscall_counts[filename][syscall_name] = 1
             else:
-                if syscall_name not in self.per_file_syscall_counts[filename]:
-                    self.per_file_syscall_counts[filename][syscall_name] = 1
-                else:
-                    self.per_file_syscall_counts[filename][syscall_name] += 1
-        else:
-            return
+                self.per_file_syscall_counts[filename][syscall_name] += 1
 
     def calculate_total_calls_per_file(self):
         for f in self.per_file_syscall_counts.keys():
@@ -73,5 +69,6 @@ class PerFileSysCallCount():
             self.per_file_syscall_counts[f]['total'] = total
 
     def write_output_file(self, output_file: str):
+        self.calculate_total_calls_per_file()
         with open(file=output_file, mode='w') as per_file_syscounts_output:
             json.dump(self.per_file_syscall_counts, per_file_syscounts_output, indent=2)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 from pathlib import Path
 from io_profiler.file_reader import TraceFileReader
 from io_profiler.global_io_stats import GlobalCSVFy, GlobalEventCount
-from io_profiler.per_file_io_stats import PerFileEvents
+from io_profiler.per_file_io_stats import PerFileEvents, PerFileSysCallCount
 
 
 def cli_args():
@@ -26,13 +26,16 @@ def main():
     global_csvfy = GlobalCSVFy(name='g-csvfy', csv_filename=cli.csv_output)
     global_evt_count = GlobalEventCount(name='g-evt-count')
     per_file_events = PerFileEvents(name='telemetry-file',tlt_dir=cli.tlt_output)
+    per_file_evt_count = PerFileSysCallCount(name='pf-evt-count')
     
     bpf_trfile_reader.subscribe(global_csvfy)
     bpf_trfile_reader.subscribe(global_evt_count)
     bpf_trfile_reader.subscribe(per_file_events)
+    bpf_trfile_reader.subscribe(per_file_evt_count)
     bpf_trfile_reader.read_file()    
 
     global_evt_count.write_output_file(output_file=cli.output_dir + '/event_count.csv')
+    per_file_evt_count.write_output_file(output_file=cli.output_dir + '/event_count_per_file.csv')
 
 
 if __name__ == '__main__':
